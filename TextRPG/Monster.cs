a5d1d1a562e9b2ad8545375c1f160ff882597e3e
KIMSHINWOO <48780618+cheerio31608@users.noreply.github.com>
2024-04-29 17:02:19 +0900

    Feat: 추가 monster 기본정보 표시 함수
    1. 몬스터 정보를 저장하는 생성자 추가
    2. 몬스터 정보를 표시하는 메서드 추가

--- TextRPG/Monster.cs
+++ TextRPG/Monster.cs
@@ -1,7 +1,53 @@
 ﻿using System;
 
 
-public class Moster
+internal class Monster
 {
-	
+    public string Name { get; } //이름
+    public float Hp { get; } //체력
+    public float Level { get; } //레벨
+    public float Attack { get; } //공격력
+    public bool IsDead { get; private set; } //몬스터 사망여부
+
+    public Monster(string name, float hp, float level, float attack, bool isDead = false)
+    {
+        Name = name;
+        Hp = hp;
+        Level = level;
+        Attack = attack;
+        IsDead = isDead;
+    }
+
+    //몬스터 상태정보 표시
+    //[번호] [레벨] [이름] [체력] 예) 1 Lv.2 미니언  HP 15
+    //사망한 몬스터는 [체력] 대신 Dead 표시
+    internal void PrintMonsterDescription(bool withNumber = false, int idx = 0)
+    {
+        if (withNumber)
+        {
+            Console.Write("{0} ", idx);
+        }
+        Console.Write(" | ");
+        Console.Write($"Lv.{Level} ");
+        Console.Write(Name);
+        if (IsDead)
+        {
+            Console.WriteLine("Dead");
+        }
+        else
+        {
+            Console.WriteLine($"HP {Hp}");
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
