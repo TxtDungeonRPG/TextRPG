d6826127c583e7bec1d48709516663993b5c0bb2
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-04-29 16:56:13 +0900

    feat 게임매니저 화면 목업작업

--- TextRPG/ConsoleUtil.cs
+++ TextRPG/ConsoleUtil.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 
-public class ConsolUtil
+public class ConsoleUtil
 {
+    // 메뉴 선택
+    public static int MenuChoice(int min, int max, string message = "")
+    {
+        while (true)
+        {
+            Console.WriteLine(message);
+            if (int.TryParse(Console.ReadLine(), out int choice) && choice >= min && choice <= max)
+            {
+                return choice;
+            }
+            Console.WriteLine("잘못된 입력입니다.");
+        }
+    }
 
 }
