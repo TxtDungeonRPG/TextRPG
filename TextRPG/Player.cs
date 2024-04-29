d4d61dfed66f251012e2e5fccf16b34171802e53
Kaldorei00910 <117884183+Kaldorei00910@users.noreply.github.com>
2024-04-29 15:18:35 +0900

    Player.cs
    해당 스크립트 내용 추가

--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -1,6 +1,21 @@
 ﻿using System;
 
-public class Palyer
+public class Player
 {
-	
+	int level;
+	string chad;
+	float atkPlayer;
+	float dfdPlayer;
+	int hp;
+	int gold;
+
+	public void SetPlayer(int _level, string _chad, float _atkPlayer,float _dfdPlayer, int _hp, int _gold)
+	{
+		level = _level;
+		chad = _chad;
+		atkPlayer = _atkPlayer;
+		dfdPlayer = _dfdPlayer;
+		hp = _hp;
+		gold = _gold;
+	}
 }
