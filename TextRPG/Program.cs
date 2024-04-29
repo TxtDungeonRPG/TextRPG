a676ab321da9236aedaf31128bca613adbba27b9
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-04-29 12:56:34 +0900

    Init

---
+++ TextRPG/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TextRPG
+{
+    public class GameManager
+    {
+        public GameManager() 
+        {
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+  
+
+        }
+
+        public void StartGame()
+        {
+
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager gameManager = new GameManager();
+            gameManager.StartGame();
+            Console.WriteLine("hi");
+        }
+    }
+}
