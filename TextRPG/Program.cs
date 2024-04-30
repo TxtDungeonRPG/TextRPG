7b92e41e5dcf8f995c68b30191f7b172ea0c1037
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-04-30 12:45:57 +0900

    Feat: 전투 몬스터 턴 기능 완료
    moster isAttack 추가

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Xml.Serialization;
 
 namespace TextRPG
@@ -24,9 +25,6 @@ namespace TextRPG
 
             //몬스터 리스트 초기값
             monsterlist = new List<Monster>();
-            //monsterlist.Add(new Monster(2, "미니언", 15, 5));
-            //monsterlist.Add(new Monster(5, "대포미니언", 25, 8));
-            //monsterlist.Add(new Monster(3, "공허충", 10, 9));
 
         }
 
@@ -122,17 +120,12 @@ namespace TextRPG
 
             Console.WriteLine("■ Battle!! ■");
             Console.WriteLine("");
-            //Console.WriteLine("Lv.2 미니언  HP 15");
-            //Console.WriteLine("Lv.5 대포미니언 HP 25");
-            //Console.WriteLine("LV.3 공허충 HP 10");
             for (int i = 0; i < monsterlist.Count; i++)
             {
                 monsterlist[i].PrintMonsterDescription(false, i + 1);
             }
             Console.WriteLine("");
             Console.WriteLine("[내정보]");
-            //Console.WriteLine("Lv.1  Chad (전사) ");
-            //Console.WriteLine("HP 100/100");
             Console.Write($"Lv.{player.Level} ");
             Console.WriteLine($"{player.Name} ({player.Class})");
             Console.WriteLine($"HP {player.Hp}/{player.MaxHp}"); 
@@ -172,34 +165,30 @@ namespace TextRPG
             Console.WriteLine("");
             Console.WriteLine("[내정보]");
             Console.Write($"Lv.{player.Level} ");
-            Console.WriteLine($"Chad ({player.Class})");
+            Console.WriteLine($"{player.Name} ({player.Class})");
             Console.WriteLine($"HP {player.Hp}/{player.MaxHp}");
             Console.WriteLine("");
             Console.WriteLine("0. 취소");
             Console.WriteLine("");
             do
             {
-                choice = ConsoleUtil.MenuChoice(0, 3, "대상을 선택해주세요.");
+                choice = ConsoleUtil.MenuChoice(0, monsterlist.Count, "대상을 선택해주세요.");
                 if (choice != 0 && monsterlist[choice - 1].IsDead)
                 {
                     Console.WriteLine("이미 죽은 몬스터입니다");
                 }
             }
             while (choice != 0 && monsterlist[choice - 1].IsDead);
-            
 
-            switch (choice)
+            if(choice == 0)
             {
-                case 1:
-                case 2:
-                case 3:
-                    Attack(choice);
-                    break;
-                case 0:
-                    StartBattleMenu();
-                    break;
+                StartBattleMenu();
+
+            }else
+            {
+                Attack(choice);
             }
-            AttackMenu();
+
         }
 
         private void Attack(int choiceEnemy)
@@ -209,7 +198,7 @@ namespace TextRPG
             Console.Clear();
             Console.WriteLine("■ Battle!! ■");
             Console.WriteLine("");
-            Console.WriteLine("{0}의 공격!",player.Chad);
+            Console.WriteLine("{0}의 공격!",player.Name);
             Console.WriteLine("Lv.{0} {1} 을(를) 맞췄습니다. [데미지 : {2}]", monsterlist[choiceEnemy-1].Level, monsterlist[choiceEnemy-1].Name,damage);
             Console.WriteLine("");
             Console.WriteLine("Lv.{0} {1}", monsterlist[choiceEnemy - 1].Level, monsterlist[choiceEnemy - 1].Name);
@@ -225,9 +214,6 @@ namespace TextRPG
                 monsterlist[choiceEnemy - 1].Hp -= damage;
             }
 
-            Console.Write($"Lv.{player.Level} ");
-            Console.WriteLine($"Chad ({player.Chad})");
-            Console.WriteLine($"HP {player.Hp}/{player.MaxHp}");
             Console.WriteLine("");
             Console.WriteLine("0. 다음");
             Console.WriteLine("");
@@ -237,28 +223,46 @@ namespace TextRPG
             switch (choice)
             {
                 case 0:
-                    EnemyPhase();
+                    // 몬스터가 모두 죽은경우 승리
+                    foreach (var monster in monsterlist)
+                    {
+                        if (!monster.IsDead)
+                        {
+                            EnemyPhase();
+                        }
+                    }
+                    Victory();
                     break;
             }
         }
 
-        private void EnemyPhase(int choiceEnemy)
+        private void EnemyPhase()
         {
-            Monster monster = monsterlist[choiceEnemy];
+            
+            Monster? monster = null;
 
-            Console.Clear();
-
-            Console.WriteLine("■ Battle!! ■");
-            Console.WriteLine("");
-
-            // 몬스터가 죽은 경우
-            if (monster.IsDead)
+            for (int i = 0; i < monsterlist.Count; i++)
             {
-                Console.WriteLine($"Lv.{monster.Level} {monster.Name} Dead");
+                // 몬스터가 살아있고 공격하지 않은 경우 공격할 몬스터 선택
+                if (!monsterlist[i].IsDead)
+                {
+                    if (!monsterlist[i].IsAttack)
+                    {
+                        monster = monsterlist[i];
+                        monster.IsAttack = true;
+                        break;
+                    }                 
+                }             
             }
-            else
+
+
+            // 공격할 몬스터가 있는 경우
+            if (monster != null)
             {
-                // 몬스터가 살아있는 경우
+                Console.Clear();
+
+                Console.WriteLine("■ Battle!! ■");
+                Console.WriteLine("");
 
                 Console.WriteLine($"Lv.{monster.Level} {monster.Name} 의 공격!");
                 Console.WriteLine($"{player.Name} 을(를) 맞췄습니다.  [데미지 : {monster.Attack}]");
@@ -268,6 +272,13 @@ namespace TextRPG
 
                 player.Hp -= (int)monster.Attack;
             }
+            else
+            {
+                // 공격할 몬스터가 없는 경우 AttackMenu() 이동, 공격 여부 초기화
+                monsterlist.ForEach(x => x.IsAttack = false);
+                AttackMenu();
+            }
+
             Console.WriteLine("");
             Console.WriteLine("0. 다음");
             Console.WriteLine("");
@@ -277,7 +288,15 @@ namespace TextRPG
             switch (choice)
             {
                 case 0:
-                    AttackMenu();
+                    //캐릭터 체력이 0 이하가 된경우 패배
+                    if(player.Hp <= 0)
+                    {
+                        Lose();
+                    }
+                    else
+                    {
+                        EnemyPhase();
+                    }
                     break;
             }
         }
